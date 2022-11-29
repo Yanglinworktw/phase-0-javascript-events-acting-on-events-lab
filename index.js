496a48604fc99652551c12e01f6e9aaf2b532ee7
Yang <hidorabmon@gmail.com>
2022-11-29 11:11:37 +1100

    first update

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dogger = document.getElementById('dodger')
+
+document.addEventListener('keydown',function(e){
+  console.log(e)
+  if(e.key === 'ArrowLeft')
+  {
+    moveDodgerLeft()
+  }
+  else if(e.key === 'ArrowRight'){
+    moveDodgerRight()
+  }
+
+})
+
+function moveDodgerLeft()
+{
+  const leftNumber = dogger.style.left.replace('px','')
+  const left = parseInt(leftNumber,10)
+  if(left>0)
+  dogger.style.left = `${left -1}px`
+
+}
+
+
+function moveDodgerRight(){
+  const leftNumber = dogger.style.left.replace('px','')
+  const left = parseInt(leftNumber,10)
+  if(left<360)
+  dogger.style.left = `${left +1}px`
+
+}
